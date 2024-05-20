268528fc57c72b984a345402e189cd5cf98f9632
B12 <blessen010@gmail.com>
2024-05-20 16:49:26 +0530

    snaps and first command

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import discord
 from discord.ext import commands
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+token = os.environ.get("TOKEN")
+
+client = commands.Bot(command_prefix = "s!", intents = discord.Intents.all())
+
+@client.event
+async def on_ready():
+    print("bot is ready")
+
+@client.command()
+async def ping(ctx):
+    await ctx.send("pong")
+
+client.run(token)
